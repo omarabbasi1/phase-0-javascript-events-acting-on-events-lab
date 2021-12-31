5544eaaef06415072219afdbb4bc679cf15f2146
omarabbasi <omarkabbasi@outlook.com>
2021-12-30 20:33:37 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Your code here
+document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft") {
+      const leftNumbers = dodger.style.left.replace("px", "");
+      const left = parseInt(leftNumbers, 10);
+  
+      dodger.style.left = `${left - 1}px`;
+    }
+  });
+
+  const dodger = document.getElementById("dodger");
+
+  function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+  function moveDodgerRight() {
+      const rightNumbers = dodger.style.left.replace("px", "");
+      const right = parseInt(rightNumbers, 10);
+
+      if (right > 0) {
+        dodger.style.left = `${left + 1}px`;
+
+  }};
